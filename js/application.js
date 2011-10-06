d5da2029c114fd5a855e2d1b0cc4f16d8f3ca8e3
Travis Winters <travo@prozacblues.com>
2011-10-06 16:37:46 +1100

    Regenerate

--- js/application.js
+++ js/application.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
 
   var uri = window.location.href;
   var page_name = _(uri.split("/")[3].split(".")).first();
-  $("a." + page_name).addClass('selected')
+  $("a." + page_name).addClass('selected');
+  
 });

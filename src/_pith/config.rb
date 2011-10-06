00cbc2969a24aabb5a742d8ff785dfa8f547291b
Dan Tropp <dan.tropp@gmail.com>
2011-10-06 15:58:17 +1100

    tweets page driven by helper

--- src/_pith/config.rb
+++ src/_pith/config.rb
@@ -2,6 +2,14 @@ require 'twitter'
 require 'time-ago-in-words'
 
 project.helpers do
+  def tweets
+    @tweets ||= Twitter.user_timeline("cogent_thoughts", :count => 10)
+  end
+
+  def tweet(index=0)
+    include("/_partials/_tweet.html.haml", :tweet => tweets[index])
+  end
+  
   def markup_links(tweet_text)
     tweet_text
   end

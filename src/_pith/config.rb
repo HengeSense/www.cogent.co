bab7ac7d5ff87f36c0dd56f52248868099a886f6
Travis Winters <travo@prozacblues.com>
2011-10-07 16:39:16 +1100

    A bunch of cross browser preparations and tweaks. Oh, and Twitter should not barf if the service is down.

--- src/_pith/config.rb
+++ src/_pith/config.rb
@@ -3,29 +3,16 @@ require 'time-ago-in-words'
 
 project.helpers do
   def tweets
-    @tweets ||= Twitter.user_timeline("cogent_thoughts", :count => 10)
+    begin
+      @tweets ||= Twitter.user_timeline("cogent_thoughts", :count => 10)
+    end
   end
 
   def tweet(index=0)
-    include("/_partials/_tweet.html.haml", :tweet => tweets[index])
+    include("/_partials/_tweet.html.haml", :tweet => tweets[index]) if @tweets
   end
   
   def markup_links(tweet_text)
     tweet_text
   end
-  # %a{ :href => "http://bit.ly/rAnD0m" }http://bit.ly/rAnD0m
-  
-  # function twitterCallback2(twitters) {
-  #   var statusHTML = [];
-  #   for (var i=0; i<twitters.length; i++){
-  #     var username = twitters[i].user.screen_name;
-  #     var status = twitters[i].text.replace(/((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g, function(url) {
-  #       return '<a href="'+url+'">'+url+'</a>';
-  #     }).replace(/\B@([_a-z0-9]+)/ig, function(reply) {
-  #       return  reply.charAt(0)+'<a href="http://twitter.com/'+reply.substring(1)+'">'+reply.substring(1)+'</a>';
-  #     });
-  #     statusHTML.push('<li><span>'+status+'</span> <a style="font-size:85%" href="http://twitter.com/'+username+'/statuses/'+twitters[i].id_str+'">'+relative_time(twitters[i].created_at)+'</a></li>');
-  #   }
-  #   document.getElementById('twitter_update_list').innerHTML = statusHTML.join('');
-  # }
 end

dca4366ccd1b9832c45488750058e3583f9d02a6
Dan Tropp <dan.tropp@gmail.com>
2011-10-10 13:52:08 +1100

    All projects and their straplines are driven from yaml file.  This is on home, project footer and projects/index.html

--- src/_pith/config.rb
+++ src/_pith/config.rb
@@ -1,6 +1,19 @@
 require 'twitter'
 
 project.helpers do
+  def projects_html(count=nil, include_images=true)
+    yaml = projects_yaml
+    yaml = yaml.first(count) if count
+    p count
+    yaml.map do |project|
+      include('/_partials/_project_summary.html.haml', :project => project, :include_images => include_images)
+    end.join
+  end
+  
+  def projects_yaml
+    YAML.load(File.read(File.expand_path(File.dirname(__FILE__) + '/projects.yml')))
+  end
+  
   def tweets
     @tweets ||= Twitter.list_timeline("dan_tropp", "cogent-team", :count => 10)
   end

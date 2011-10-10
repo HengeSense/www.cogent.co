b042e6668d9f302f27c5f2db91bdd6c5b5f90910
Dan Tropp <dan.tropp@gmail.com>
2011-10-10 16:40:44 +1100

    Project photos now coming from flickr sets, including title and linking out to flickr photosets.

--- src/_pith/config.rb
+++ src/_pith/config.rb
@@ -1,8 +1,28 @@
 require 'twitter'
+require 'flickraw'
 
 project.helpers do
+  FlickRaw.api_key="8a82b5a4074ce3cea539edf10405aa0a"
+  FlickRaw.shared_secret="11e0c00eb37d684b"
+  def flickr_photo_in_set_url(owner, photo, set)
+     "http://www.flickr.com/photos/#{owner}/#{photo}/in/set-#{set}/"
+  end
+  
+  def project_photos(project, count=1)
+    photoset_id = projects_yaml[project]['photoset_id']
+    return [] unless photoset_id
+    photoset = flickr.photosets.getPhotos(:photoset_id => photoset_id, :extras => ['title'], :per_page => count)
+    photoset.photo.map do |p|
+      { :url => FlickRaw.url(p), :caption => p.title, :set_url => flickr_photo_in_set_url(photoset.owner, p.id, photoset_id) } 
+    end
+  end
+  
+  def project_photo_html(photo_info)
+    include('/_partials/_project_photo.html.haml', :photo => photo_info) if photo_info
+  end
+  
   def projects_html(count=nil, include_images=true)
-    yaml = projects_yaml
+    yaml = projects_yaml.values
     yaml = yaml.first(count) if count
     p count
     yaml.map do |project|
@@ -11,15 +31,15 @@ project.helpers do
   end
   
   def projects_yaml
-    YAML.load(File.read(File.expand_path(File.dirname(__FILE__) + '/projects.yml')))
+    @project_yaml ||= YAML.load(File.read(File.expand_path(File.dirname(__FILE__) + '/projects.yml')))
   end
   
   def tweets
     @tweets ||= Twitter.list_timeline("dan_tropp", "cogent-team", :count => 10)
   end
 
-  def tweet(index=0)
-    tweets.length > index ? include("/_partials/_tweet.html.haml", :tweet => tweets[index]) : ""
+  def tweet(offset=0)
+    tweets.length > offset ? include("/_partials/_tweet.html.haml", :tweet => tweets[offset]) : ""
   end
   
   def markup_links(tweet_text)

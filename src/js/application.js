ade53824e7b2e1bac55a29cbbca9d3079d02cfa5
Travis Winters <travo@prozacblues.com>
2011-10-13 10:42:49 +1100

    Lets use a jquery library to do our time ago in words. Our links to the toots should include a toot id.

--- src/js/application.js
+++ src/js/application.js
@@ -1,42 +1,12 @@
-//From twitter to convert to human time string - %script{ :src=>"http://twitter.com/javascripts/blogger.js" }
-function relative_time(time_value) {
-  var values = time_value.split(" ");
-  time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
-  var parsed_date = Date.parse(time_value);
-  var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
-  var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
-  delta = delta + (relative_to.getTimezoneOffset() * 60);
-
-  if (delta < 60) {
-    return 'less than a minute ago';
-  } else if(delta < 120) {
-    return 'about a minute ago';
-  } else if(delta < (60*60)) {
-    return (parseInt(delta / 60)).toString() + ' minutes ago';
-  } else if(delta < (120*60)) {
-    return 'about an hour ago';
-  } else if(delta < (24*60*60)) {
-    return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
-  } else if(delta < (48*60*60)) {
-    return '1 day ago';
-  } else {
-    return (parseInt(delta / 86400)).toString() + ' days ago';
-  }
-}
-
 function highlight_selected_menu_item() {
   var uri = window.location.href;
   var page_name = _(uri.split("/")[3].split(".")).first();
   $("a." + page_name).addClass('selected');
 }
 
-function make_timestamps_human() {
-  $("span.time").each(function(i) {$(this).text(relative_time($(this).text()))});
-}
-
 $(document).ready(function() {
   highlight_selected_menu_item();
-  make_timestamps_human();
+  $("span.time").timeago();
 });
 
 

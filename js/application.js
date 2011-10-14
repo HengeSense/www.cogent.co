1d254871620a233a0defc44548415ed9b38dd1b2
TreeHouse CI <builder@oak.local>
2011-10-14 16:31:54 +1100

    Regenerate

--- js/application.js
+++ js/application.js
@@ -1,12 +1,10 @@
-function highlight_selected_menu_item() {
+function highlightSelectedMenuItem() {
   var uri = window.location.href;
   var page_name = _(uri.split("/")[3].split(".")).first();
   $("a." + page_name).addClass('selected');
 }
 
-$(document).ready(function() {
-  highlight_selected_menu_item();
+$(window).load(function () {
+  highlightSelectedMenuItem();
   $("span.time").timeago();
 });
-
-

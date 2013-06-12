a04ce38103f6c65561ccbe6f0a6569704c8f7ea3
Mike Williams <mike@cogentconsulting.com.au>
2013-06-12 22:33:15 +1000

    Upgrade twitter gem; arrange for OAuth authentication.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem "haml", "~> 3.0.4"
 gem "pith", "~> 0.2.1"
 gem "rake"
 gem "RedCloth", "~> 4.2.3"
-gem "twitter", "~> 1.7.2"
+gem "twitter", "~> 4.8.0"
 gem "flickraw", "0.9"
 gem "json"
 
--- lib/twitter_feed.rb
+++ lib/twitter_feed.rb
@@ -1,17 +1,21 @@
 module TwitterFeed
-  
+
   def twitter_url(twitter_id)
     "http://twitter.com/#!/#{twitter_id}"
   end
-  
+
   def tweets
-    begin
-      @tweets ||= Twitter.list_timeline("dan_tropp", "cogent-team", :count => 10)
-    rescue Exception => ex
+    @tweets ||= begin
+      Twitter.configure do |config|
+        config.consumer_key = ENV.fetch("TWITTER_OAUTH_CONSUMER_KEY")
+        config.consumer_secret = ENV.fetch("TWITTER_OAUTH_CONSUMER_SECRET")
+      end
+      Twitter.list_timeline("dan_tropp", "cogent-team", :count => 10)
+    rescue => ex
       if ignore_feed_errors?
         raise ex
       else
-        @tweets = []
+        []
       end
     end
   end
@@ -21,10 +25,10 @@ module TwitterFeed
   end
 
   def markup_links(tweet_text)
-    linked_text = tweet_text.gsub(/((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/) do |url| 
+    linked_text = tweet_text.gsub(/((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/) do |url|
       %{<a href="#{url}">#{url}</a>}
     end
-    linked_text = linked_text.gsub(/\B@([_a-z0-9]+)/i) do |user| 
+    linked_text = linked_text.gsub(/\B@([_a-z0-9]+)/i) do |user|
       username = user.slice(1..-1)
       %{@<a href="http://twitter.com/#{username}">#{username}</a>}
     end

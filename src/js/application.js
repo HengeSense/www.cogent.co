b90ce804c535ee96fc2543d451c4e9c4681b19d1
Warren Oliver <woliver@cogentconsulting.com.au>
2013-08-09 12:40:59 +1000

    Finished styling for about page

--- src/js/application.js
+++ src/js/application.js
@@ -7,7 +7,27 @@ if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) 
   } 
 }
 
+// $.fn.stickyPosition = function() {
+//   var element = $(this);
+//   var elementOffset = $("section:visible:first").offset().top;
+//   $(window).scroll(function(e) { 
+//     if ($(window).scrollTop() >= elementOffset) {
+//       console.log("fixed");
+//       console.log(element);
+//       element.show();
+//       element.css("position", "fixed");
+//       element.css("top", "0");
+//       element.css("z-index", "1000");
+//     } else {
+//       console.log("relative");
+//       element.css("position", "relative");  
+//     }
+//   });
+// }
+
+
 $(document).ready(function() {
+  // $(".container:first").stickyPosition();
   $(".products a.show-more").click(function(e) {
     $(".products-grid a.optional").removeClass("optional");
     $(e.target).hide();

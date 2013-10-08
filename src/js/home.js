d682b8119985fc1b2eddb1a83a426e616a74ef0c
Victor Nava <victornavav@gmail.com>
2013-10-08 17:01:35 +1100

    Slider done

--- src/js/home.js
+++ src/js/home.js
@@ -6,17 +6,34 @@ $(function() {
     imageAlignCenter: false,
     addActiveClass: true,
     slideSpacing: 10,
-      visibleNearby: {
+    sliderDrag:false,
+    navigateByCenterClick: false,
+    navigateByClick: true,
+    visibleNearby: {
       enabled: true,
       centerArea: 0.6,
       center: false,
       breakpoint: 650,
       breakpointCenterArea: 0.5,
-      navigateByCenterClick: true,
-      navigateByClick: false
     }
   }).data('royalSlider');
 
   $('.slider-next').click(function(){slider.next()});
   $('.slider-prev').click(function(){slider.prev()});
+  $('.slider-prev').hide();
+
+  slider.ev.on('rsAfterSlideChange', function(event) {
+    var $next = $('.slider-next');
+    var $prev = $('.slider-prev');
+    if (slider.currSlideId <= 0) {
+      $prev.hide();
+      $next.show();
+    } else if (slider.currSlideId > 0 && slider.currSlideId < (slider.numSlides - 1)){
+      $prev.show();
+      $next.show();
+    } else {
+      $prev.show();
+      $next.hide();
+    }
+  });
 });

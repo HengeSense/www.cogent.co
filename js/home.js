0b4fa68559fc100909f946cb846f5e041d81da7b
TreeHouse CI <builder@oak.local>
2013-12-05 12:06:57 +1100

    Regenerate

---
+++ js/home.js
@@ -0,0 +1,48 @@
+$(function() {
+  var slider =  $(".royalSlider").royalSlider({
+    loopRewind: true,
+    autoScaleSlider: true,
+    keyboardNavEnabled: true,
+    imageAlignCenter: false,
+    addActiveClass: true,
+    slideSpacing: 10,
+    sliderDrag:false,
+    navigateByCenterClick: false,
+    navigateByClick: false,
+    visibleNearby: {
+      enabled: true,
+      centerArea: 0.6,
+      center: false,
+      breakpoint: 650,
+      breakpointCenterArea: 0.5,
+    }
+  }).data('royalSlider');
+
+  $('.slider-next').click(function(){slider.next()});
+  $('.slider-prev').click(function(){slider.prev()});
+  $('.slider-prev').hide();
+
+  slider.ev.on('rsAfterSlideChange', function(event) {
+    var $next = $('.slider-next');
+    var $prev = $('.slider-prev');
+    if (slider.currSlideId <= 0) {
+      $prev.hide();
+      $next.show();
+    } else if (slider.currSlideId > 0 && slider.currSlideId < (slider.numSlides - 1)){
+      $prev.show();
+      $next.show();
+    } else {
+      $prev.show();
+      $next.hide();
+    }
+  });
+
+  // TODO try to find a better way to do this. maybe ad an anchor dynamically?
+  // Make slider image behave like a link if it is the active one
+  $(".royalSlider").on("click", ".jsLink", function(){
+      var isTheActiveSlide = $(this).parents('.rsActiveSlide').length >= 1;
+      if (isTheActiveSlide) {
+        window.location.href = $(this).attr('data-href');
+      }
+  });
+});
